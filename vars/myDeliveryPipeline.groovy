4363e675114dcf005061f80033c1d575a5b610b7
Waseembas <47210393+Waseembas@users.noreply.github.com>
2022-03-22 03:36:30 -0700

    Update myDeliveryPipeline.groovy

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -3,18 +3,33 @@ def call(Map pipelineParams) {
     pipeline {
         agent any
 
+        
+      environment {
+    
+      GIT_REPO_URL = "${pipelineParams.GIT_REPO_URL}"
+      GIT_BRANCH = "${pipelineParams.GIT_BRANCH}"
+    
+     
+    }
 
 
 
         stages {
-            stage('checkout git') {
-                steps {
-                    git branch: pipelineParams.branch, credentialsId: 'GitCredentials', url: pipelineParams.scmUrl
-                }
-            }
-}
+         
+
+        
+        
+        stage('Source') {
+        steps {
+          sh "printenv | sort"
+          sh "rm -rf * && rm -rf .git"
+          git branch: "${env.GIT_BRANCH}", url:  "${env.GIT_REPO_URL}"
+        }
+      }
 
 
 }
 
+}
+    
 }
