458530b09d1c5d44c8bd264e7b10277d7a2538b9
Waseembas <47210393+Waseembas@users.noreply.github.com>
2022-03-22 03:27:52 -0700

    Add files via upload

---
+++ vars/myDeliveryPipeline.groovy
@@ -0,0 +1,20 @@
+def call(Map pipelineParams) {
+
+    pipeline {
+        agent any
+
+
+
+
+        stages {
+            stage('checkout git') {
+                steps {
+                    git branch: pipelineParams.branch, credentialsId: 'GitCredentials', url: pipelineParams.scmUrl
+                }
+            }
+}
+
+
+}
+
+}
